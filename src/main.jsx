c1fd86e7a61ed05cf6fea36ac5fabffe53444d97
edifylabsinfo-ops <edifylabs.info@gmail.com>
2026-04-20 11:49:17 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App'
+import './index.css' // QUAN TRỌNG: Dòng này phải có để web hiện màu!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
