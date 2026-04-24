b235aa58f309513dff0625fcfc7d2d033c9a6538
edifylabsinfo-ops <edifylabs.info@gmail.com>
2026-04-24 14:23:25 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,30 @@ import Quiz from './components/Quiz.jsx'
 import LeadForm from './components/LeadForm.jsx'
 import Footer from './components/Footer.jsx'
 
+// Thêm 4 dòng import này lên đầu
+import Partners from './components/Partners.jsx'
+import Team from './components/Team.jsx'
+import Blog from './components/Blog.jsx'
+import Faq from './components/Faq.jsx'
+
 // Import CSS và hiệu ứng
 import './index.css'
 
 export default function App() {
   return (
-    <>
-      <Topbar />
-      <Header />
-      <main>
-        <Hero />
-        <Metrics />
-        <Solutions />
-        <Portfolio />
-        <Quiz />
-        <LeadForm />
-      </main>
-      <Footer />
-    </>
-  )
-}
+  <main className="bg-[#163020]">
+    <Topbar />
+    <Header />
+    <Hero />
+    <Partners /> {/* Mới */}
+    <Metrics />
+    <Solutions />
+    <Team />     {/* Mới */}
+    <Portfolio />
+    <Blog />     {/* Mới */}
+    <Quiz />
+    <Faq />      {/* Mới */}
+    <LeadForm />
+    <Footer />
+  </main>
+)
