bacb0bf9f78859c3bef94c375db4ded9b43c0f91
edifylabsinfo-ops <edifylabs.info@gmail.com>
2026-04-24 16:15:59 +0700

    Reorganize imports and update component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,34 @@
-import React from 'react'
-import Topbar from './components/Topbar.jsx'
-import Header from './components/Header.jsx'
-import Hero from './components/Hero.jsx'
-import Metrics from './components/Metrics.jsx'
-import Solutions from './components/Solutions.jsx'
-import Portfolio from './components/Portfolio.jsx'
-import Quiz from './components/Quiz.jsx'
-import LeadForm from './components/LeadForm.jsx'
-import Footer from './components/Footer.jsx'
-
-// Thêm 4 dòng import này lên đầu
+import Topbar   from './components/Topbar.jsx'
+import Header   from './components/Header.jsx'
+import Hero     from './components/Hero.jsx'
 import Partners from './components/Partners.jsx'
-import Team from './components/Team.jsx'
-import Blog from './components/Blog.jsx'
-import Faq from './components/Faq.jsx'
-
-// Import CSS và hiệu ứng
+import Metrics  from './components/Metrics.jsx'
+import Solutions from './components/Solutions.jsx'
+import Team     from './components/Team.jsx'
+import Portfolio from './components/Portfolio.jsx'
+import Blog     from './components/Blog.jsx'
+import Quiz     from './components/Quiz.jsx'
+import Faq      from './components/Faq.jsx'
+import LeadForm from './components/LeadForm.jsx'
+import Footer   from './components/Footer.jsx'
 import './index.css'
 
 export default function App() {
   return (
-  <main className="bg-[#163020]">
-    <Topbar />
-    <Header />
-    <Hero />
-    <Partners /> {/* Mới */}
-    <Metrics />
-    <Solutions />
-    <Team />     {/* Mới */}
-    <Portfolio />
-    <Blog />     {/* Mới */}
-    <Quiz />
-    <Faq />      {/* Mới */}
-    <LeadForm />
-    <Footer />
-  </main>
-)
+    <main className="bg-[#163020]">
+      <Topbar />
+      <Header />
+      <Hero />
+      <Partners />
+      <Metrics />
+      <Solutions />
+      <Team />
+      <Portfolio />
+      <Blog />
+      <Quiz />
+      <Faq />
+      <LeadForm />
+      <Footer />
+    </main>
+  )
+}
